76c3dbf96f95293d2f1145262f4cb30f125db6bc
careymwarabu <careyakmwarabu@gmail.com>
2021-06-21 22:16:18 +0300

    add app/main

---
+++ app/main/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField, StringField, SelectField
+from wtforms.validators import Required, Email, EqualTo
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+    
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Pitch Category', choices=[('Busines','Business'),('Entertainment','Entertainmnet')
+    ,('Promotion','Promotion')],validators=[Required()])
+    post = TextAreaField('Pitch', validators=[Required()])
+    submit = SubmitField('Post Pitch')
+
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Comment')
